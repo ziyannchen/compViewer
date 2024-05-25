a88cabe0efdc5922aed55ecab98c0e7a34a5f9ea
ziyannchen <celinechan@mail.sdu.edu.cn>
2024-05-25 14:54:24 +0800

    v1.51 add support for quick editing of window config in ui

--- config/__init__.py
+++ config/__init__.py
@@ -7,15 +7,22 @@ class Config:
         config = yaml_load(cfg_path)
         for key, value in config.items():
             setattr(self, key, value)
+        self.onChanged()
 
     def set(self, obj):
         for attr, value in obj.items():
             if not isinstance(getattr(self, attr), str):
                 value = ast.literal_eval(value)
             setattr(self, attr, value)
+        self.onChanged()
+    
+    def onChanged(self):
+        pass
 
-windowConfig = Config('config/window.yaml')
-windowConfig.CACHE_DIR = os.path.expanduser(windowConfig.CACHE_DIR)
+class WindowConfig(Config):
+    def onChanged(self):
+        self.CACHE_DIR = os.path.expanduser(self.CACHE_DIR)
+        for key, value in self.SUPPORTED_FILES.items():
+            self.SUPPORTED_FILES[key] = tuple(value)
 
-for key, value in windowConfig.SUPPORTED_FILES.items():
-    windowConfig.SUPPORTED_FILES[key] = tuple(value)
+windowConfig = WindowConfig('config/window.yaml')
